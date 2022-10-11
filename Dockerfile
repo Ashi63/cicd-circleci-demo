0ae99a95bdc17a5b557afec0cca413ecdcfc1be8
Ashi63 <ashish.ashi6388@gmail>
2022-10-11 12:21:42 +0530

    updated to original

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM python:3.7
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-ENTRYPOINT ["python"]
-CMD ["app.py"]
-
+ENTRYPOINT [ "python" ]
+CMD [ "main.py" ]
--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
-def index():
-    return "Flask app is running in heroku"
 
-if __name__== '__main__':
-    app.run()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Flask app is running"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
